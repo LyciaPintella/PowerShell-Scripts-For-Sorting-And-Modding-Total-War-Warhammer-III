2f2259f3ee7cce8185952b0b71c7cc78de3ee291
Lycia Pintella <jessica.ariadne.murphy@gmail.com>
2025-05-03 10:19:07 -0400

    Hi

--- Sybolically Link Powershell Configurations Between User Accounts.ps1
+++ Sybolically Link Powershell Configurations Between User Accounts.ps1
@@ -1,2 +1,7 @@
-cd "c:\Users\Lycia Pintella\AppData\Local\Packages\"
-New-Item -ItemType HardLink -Path "Windows Terminal Shell Configuration.json" -Target "c:\Users\Jessica Murphy\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Set-Location "c:\Users\Lycia Pintella\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe"
+cmd /c rmdir /s /q "LocalState"
+New-Item -ItemType Junction -Path "LocalState" -Target "c:\Users\Jessica Murphy\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+
+Set-Location "c:\Users\Rose Pintella\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe"
+cmd /c rmdir /s /q "LocalState"
+New-Item -ItemType Junction -Path "LocalState" -Target "c:\Users\Jessica Murphy\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
