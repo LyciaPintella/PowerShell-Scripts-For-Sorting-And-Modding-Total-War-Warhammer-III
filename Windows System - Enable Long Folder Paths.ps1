8e61dce94a8cc7d88f74f18239273f958a09a2eb
Lycia Pintella <jessica.ariadne.murphy@gmail.com>
2025-05-03 10:19:34 -0400

    Hi

--- Windows System - Enable Long Folder Paths.ps1
+++ Windows System - Enable Long Folder Paths.ps1
@@ -1,2 +1,4 @@
-﻿New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD –Force | Out-Null
+﻿#Fixes issue documented at: https://www.reddit.com/r/MDT/comments/9nd31x/comment/e7lox4c/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1
+
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD –Force | Out-Null
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD –Force | Out-Null
