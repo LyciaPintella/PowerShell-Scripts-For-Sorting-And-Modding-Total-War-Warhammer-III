c1a19ea86334b1419d858db11e4e44074aa87a66
Jessica Ariadne Murphy <Jessica.Ariadne.Murphy@Gmail.com>
2025-08-17 13:50:19 -0400

    Hi

--- PowerShell Commands.ps1
+++ PowerShell Commands.ps1
@@ -1,24 +1,25 @@
-<# "C:\OneDrive\Jessica Ariadne Murphy\OneDrive\OneDrive Files\Total War Warhammer III - OneDrive Backups\Total War Warhammer III - PowerShell Scripts" continues #>
+<#"C:\OneDrive\JessicaAriadneMurphy\OneDrive\OneDriveFiles\TotalWarWarhammerIII-OneDriveBackups\TotalWarWarhammerIII-PowerShellScripts"continues#>
 
-Get-ChildItem "C:\OD\Jessica\OneDrive\Jess Files\Games\Steam\Total War\Pharaoh Dynasties\data" -Filter *.pack -Recurse | Select-String "hcp_character_created" | Select-Object -Unique Path | Format-Wide
+Get-ChildItem"C:\OD\Jessica\OneDrive\JessFiles\Games\Steam\TotalWar\PharaohDynasties\data"-Filter*.pack-Recurse | Select-String"hcp_character_created" | Select-Object-UniquePath
 
-Get-ChildItem "C:\OD\Jessica\OneDrive\Jess Files\Games\Steam\Total War\Steam Workshop Mods\Pharaoh Dynasties" -Filter *.pack -Recurse | Select-String "hcp_character_created" | Select-Object -Unique Path | Format-Wide
+Get-ChildItem"C:\OD\Jessica\OneDrive\JessFiles\Games\Steam\TotalWar\SteamWorkshopMods\PharaohDynasties"-Filter*.pack-Recurse | Select-String"hcp_character_created" | Select-Object-UniquePath
 
-# Directory List Short Name
-Get-ChildItem -Name
-Get-ChildItem -Name Format-Wide
+#DirectoryListShortName
+Get-ChildItem-Name
+Get-ChildItem-NameFormat-Wide
 
-# Directory List Wide Format
+#DirectoryListWideFormat
 
 Get-ChildItem | Format-Wide
 
-# Directory List Full Name
-Get-ChildItem | Select-Object -ExpandProperty FullName | Format-Wide
+#DirectoryListFullName
+Get-ChildItem | Select-Object -ExpandPropertyFullName
+Get-ChildItem | Select-Object -ExpandPropertyFullName | Format-Wide
 
-#Find all junctions under this folder
-Get-ChildItem -Path "C:\OneDrive" -Recurse -Force | Where-Object { $_.LinkType -ne $null -or $_.Attributes -match "Junction" } | ft FullName,Linktype,Target | Format-Wide
-Get-ChildItem -Path "C:\OneDrive\Jessica Ariadne Murphy\OneDrive" -Recurse -Force | Where-Object { $_.LinkType -ne $null -or $_.Attributes -match "Junction" } | ft FullName,Linktype,Target | Format-Wide
+#Findalljunctionsunderthisfolder
+Get-ChildItem-Path"C:\OneDrive"-Recurse-Force | Where-Object{$_.LinkType-ne$null-or$_.Attributes-match"Junction"} | ftFullName,Linktype,Target | Format-Wide
+Get-ChildItem-Path"C:\OneDrive\JessicaAriadneMurphy\OneDrive"-Recurse-Force | Where-Object{$_.LinkType-ne$null-or$_.Attributes-match"Junction"} | ftFullName,Linktype,Target | Format-Wide
 
-#This does it from the current directory
-Get-ChildItem -Recurse -Force | Where-Object { $_.LinkType -ne $null -or $_.Attributes -match "Junction" } | ft FullName,Linktype,Target | Format-Wide
+#Thisdoesitfromthecurrentdirectory
+Get-ChildItem-Recurse-Force | Where-Object{$_.LinkType-ne$null-or$_.Attributes-match"Junction"} | ftFullName,Linktype,Target
 
