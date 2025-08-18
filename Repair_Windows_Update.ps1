bbade329b7567fee25a42c6e0ec5b19b90121787
Jessica Ariadne Murphy <Jessica.Ariadne.Murphy@Gmail.com>
2025-08-18 06:21:32 -0400

    Hi

--- Repair_Windows_Update.ps1
+++ Repair_Windows_Update.ps1
@@ -1,3 +1,19 @@
+# Repair Windows Update Script
+
+# This script is designed to repair Windows Update issues by resetting components, cleaning up files, and restoring the Start Menu Programs.
+
+#! KEEP A BACKUP OF THE START MENU CONFIGURATION
+Remove-Item -Path "C:\OD\Jessica\OneDrive\Jess Files\Start Menu ProgramData Backup\Programs" -Recurse -Force
+New-Item -Path "C:\OD\Jessica\OneDrive\Jess Files\Start Menu ProgramData Backup\Programs" -ItemType Directory
+Copy-Item -Path "C:\OD\Jessica\OneDrive\Jess Files\Windows Tools And Drivers\Start Menu ProgramData Backup\*" -Destination "C:\OD\Jessica\OneDrive\Jess Files\Start Menu ProgramData Backup" -Recurse -Force
+
+#! KEEP ANOTHER FUCKING BACKUP OF THE START MENU CONFIGURATION
+Remove-Item -Path "F:\Start Menu ProgramData Backup\Programs" -Recurse -Force
+New-Item -Path "F:\Start Menu ProgramData Backup\Programs" -ItemType Directory
+Copy-Item -Path "C:\OD\Jessica\OneDrive\Jess Files\Windows Tools And Drivers\Start Menu ProgramData Backup\*" -Destination "F:\Start Menu ProgramData Backup" -Recurse -Force
+
+#? End Backup Section
+
 Remove-Item -Path "C:\OD\Jessica\OneDrive\Jess Files\Windows Tools And Drivers\Start Menu ProgramData Backup\Programs" -Recurse -Force
 Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Destination "C:\OD\Jessica\OneDrive\Jess Files\Windows Tools And Drivers\Start Menu ProgramData Backup" -Recurse -Force
 Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Recurse -Force
@@ -10,7 +26,7 @@ DISM /Online /Cleanup-Image /RestoreHealth
 
 sfc /scannow
 
-# DISM /Online /Cleanup-Image /RestoreHealth /Source:L:\sources\install.wim:1 /LimitAccess
+#? DISM /Online /Cleanup-Image /RestoreHealth /Source:L:\sources\install.wim:1 /LimitAccess
 net stop wuauserv
 net stop cryptSvc
 net stop bits
@@ -25,22 +41,18 @@ net start bits
 
 netsh winsock reset
 
-# Install updates without automatic reboot
+#? Install updates without automatic reboot
 Import-Module PSWindowsUpdate
 Install-WindowsUpdate -AcceptAll -IgnoreReboot
 
-# PAUSE HERE UNTIL REBOOT IS DONE.
+#! PAUSE HERE UNTIL REBOOT IS DONE.
 
 # After Windows Update, restore the Start Menu Programs
 Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Recurse -Force
 New-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -ItemType Directory
 Copy-Item -Path "C:\OD\Jessica\OneDrive\Jess Files\Windows Tools And Drivers\Start Menu ProgramData Backup\*" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu" -Recurse -Force
 
-
-
-
-
-# Optional tools
+<# Optional tools
 
 f:\setup.exe /auto upgrade /dynamicupdate disable /compat ignorewarning /migratedrivers all
 
@@ -48,3 +60,4 @@ Start-Process ms-settings:recovery
 
 # reboot into recovery mode
 shutdown /r /o /f /t 00
+#>
